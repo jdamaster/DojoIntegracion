bbbdad189e54f5ebab463767ae6101fdcc0d5a58
jdamaster <julianx8.holgin@gmail.com>
2016-05-07 11:11:15 -0500

    Prueba corregida

--- test/appempic_test.js
+++ test/appempic_test.js
@@ -6,7 +6,7 @@ var assert = require('should');
 describe('appempic', function () {
 
   it('El nombre deberia ser ', function () {
-    appempic.getName().should.equal('Julian1');
+    appempic.getName().should.equal('Julian');
   });
   it('El apellido deberia ser ', function () {
     appempic.getLastName().should.equal('Arango');
